57ff0c661c529bb18a7d439689f3ce5ec578d01c
obezbolen67 <131885955+obezbolen67@users.noreply.github.com>
2024-08-31 15:38:34 +0300

    Update models.js

--- src/models.js
+++ src/models.js
@@ -1,12 +1,18 @@
 const modelMap = {
   "gpt-3.5-turbo": "gemini-1.5-flash-exp-0827",
-  "gpt-4": "gemini-1.5-pro-exp-0827",
+  "gpt-4": "gemini-1.5-pro-exp-0801",
   "gpt-4-turbo": "gemini-1.5-flash-exp-0827",
-  "gpt-4o": "gemini-1.5-flash-exp-0827",
-  "gpt-4o-mini": "gemini-1.5-pro-exp-0827",
+  "gpt-4o": "gemini-1.5-pro-exp-0801",
+  "gpt-4o-mini": "gemini-1.5-flash-exp-0827",
 };
 
 const modelsList = [
+  {
+    created: 1677610602,
+    object: "model",
+    owned_by: "google",
+    id: "gemini-1.5-pro-exp-0801",
+  },
   {
     created: 1677610602,
     object: "model",
