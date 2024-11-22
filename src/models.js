87b5f936149d23d7db60c2b83ffed8c754ecfa7b
obezbolen67 <131885955+obezbolen67@users.noreply.github.com>
2024-11-22 20:18:11 +0200

    Added gemini-exp-1121

--- src/models.js
+++ src/models.js
@@ -1,8 +1,8 @@
 const modelMap = {
   "gpt-3.5-turbo": "gemini-1.5-flash-latest",
-  "gpt-4": "gemini-exp-1114",
+  "gpt-4": "gemini-exp-1121",
   "gpt-4-turbo": "gemini-1.5-flash-latest",
-  "gpt-4o": "gemini-exp-1114",
+  "gpt-4o": "gemini-exp-1121",
   "gpt-4o-mini": "gemini-1.5-flash-latest",
 };
 
@@ -11,7 +11,7 @@ const modelsList = [
     created: 1677610602,
     object: "model",
     owned_by: "google",
-    id: "gemini-exp-1114",
+    id: "gemini-exp-1121",
   },
   {
     created: 1677610602,
