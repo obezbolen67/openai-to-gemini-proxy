771a4d21fefc9db43d83f145c3124273461679fd
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 <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>
2024-12-20 19:50:19 +0200

    Removed outdated models.

--- src/models.js
+++ src/models.js
@@ -1,6 +1,6 @@
 const modelMap = {
   "gpt-4": "gemini-1.5-pro-latest",
-  "gpt-4o": "gemini-exp-1206",
+  "gpt-4o": "gemini-2.0-flash-exp",
   "gpt-4o-mini": "gemini-2.0-flash-exp",
   "o1-preview": "gemini-2.0-flash-thinking-exp"
 };
@@ -16,43 +16,13 @@ const modelsList = [
     created: 1677610602,
     object: "model",
     owned_by: "google",
-    id: "gemini-exp-1206",
-  },
-  {
-    created: 1677610602,
-    object: "model",
-    owned_by: "google",
     id: "gemini-2.0-flash-exp",
   },
   {
     created: 1677610602,
     object: "model",
     owned_by: "google",
-    id: "gemini-exp-1121",
-  },
-  {
-    created: 1677610602,
-    object: "model",
-    owned_by: "google",
-    id: "gemini-exp-1114",
-  },
-  {
-    created: 1677610602,
-    object: "model",
-    owned_by: "google",
-    id: "gemini-1.5-pro-exp-0827",
-  },
-  {
-    created: 1677610602,
-    object: "model",
-    owned_by: "google",
     id: "gemini-1.5-pro-latest",
-  },
-  {
-    created: 1677610602,
-    object: "model",
-    owned_by: "google",
-    id: "gemini-1.5-flash-exp-0827",
   },
   {
     created: 1677610602,
