973afe9fc9b75f585f22429533f9ebbfc9045ced
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 <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>
2024-12-20 19:13:54 +0200

    #

--- src/models.js
+++ src/models.js
@@ -2,9 +2,16 @@ const modelMap = {
   "gpt-4": "gemini-1.5-pro-latest",
   "gpt-4o": "gemini-exp-1206",
   "gpt-4o-mini": "gemini-2.0-flash-exp",
+  "o1-preview": "gemini-2.0-flash-thinking-exp"
 };
 
 const modelsList = [
+  {
+    created: 1677610602,
+    object: "model",
+    owned_by: "google",
+    id: "gemini-2.0-flash-thinking-exp",
+  },
   {
     created: 1677610602,
     object: "model",
