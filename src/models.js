4eb38ad91be2565dea7f61f1e730082caf2d0843
obezbolen67 <131885955+obezbolen67@users.noreply.github.com>
2025-06-03 16:24:37 +0300

    Update models.js

--- src/models.js
+++ src/models.js
@@ -1,9 +1,9 @@
 const modelMap = {
-  "gpt-4": "gemini-2.0-pro-exp-02-05",
-  "gpt-4o": "gemini-2.5-pro-exp-03-25",
-  "gpt-4o-mini": "gemini-2.0-flash-exp",
-  "o1-preview": "gemini-2.0-flash-thinking-exp-01-21",
-  "o1": "gemini-2.5-pro-exp-03-25"
+  "gpt-4": "gemini-2.5-pro-preview-05-06",
+  "gpt-4o": "gemini-2.5-pro-preview-05-06",
+  "gpt-4o-mini": "gemini-2.5-flash-preview-05-20",
+  "o1-preview": "gemini-2.5-pro-preview-05-06",
+  "o1": "gemini-2.5-pro-preview-05-06"
 };
 
 const modelsList = [
@@ -11,38 +11,14 @@ const modelsList = [
     created: 1677610602,
     object: "model",
     owned_by: "google",
-    id: "gemini-2.5-pro-exp-03-25",
+    id: "gemini-2.5-flash-preview-05-20",
   },
   {
     created: 1677610602,
     object: "model",
     owned_by: "google",
-    id: "gemini-2.0-flash-thinking-exp-01-21",
+    id: "gemini-2.5-pro-preview-05-06",
   },
-  {
-    created: 1677610602,
-    object: "model",
-    owned_by: "google",
-    id: "gemini-2.0-pro-exp-02-05",
-  },
-  {
-    created: 1677610602,
-    object: "model",
-    owned_by: "google",
-    id: "gemini-2.0-flash-exp",
-  },
-  {
-    created: 1677610602,
-    object: "model",
-    owned_by: "google",
-    id: "gemini-1.5-pro-latest",
-  },
-  {
-    created: 1677610602,
-    object: "model",
-    owned_by: "google",
-    id: "gemini-1.5-flash-latest",
-  }
 ];
 
 export { modelsList, modelMap };
